97244199c05b929941ff0c36d9ecc80f50d433d4
nate <nrice@truecoders.io>
2019-12-02 18:11:12 -0600

    init commit

---
+++ Inheritance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO Be sure to follow best practice when creating your classes
+
+            // Create a class Animal
+            // give this class 4 members that all Animals have in common
+
+
+            // Create a class Bird
+            // give this class 4 members that are specific to Bird
+            // Set this class to inherit from your Animal Class
+
+            // Create a class Reptile
+            // give this class 4 members that are specific to Reptile
+            // Set this class to inherit from your Animal Class
+
+
+
+
+            /*Create an object of your Bird class
+             *  give values to your members using the object of your Bird class
+             *  
+             * Creatively display the class member values 
+             */
+
+            /*Create an object of your Reptile class
+             *  give values to your members using the object of your Bird class
+             *  
+             * Creatively display the class member values 
+             */
+        }
+    }
+}
